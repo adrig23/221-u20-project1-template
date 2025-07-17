ade5abe680be4a4d94ad465b58d9ad078d7e33a6
Adriana Galaviz <ag5248@students.necsd.net>
2025-07-17 15:46:51 -0400

    feed.js added

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -3,10 +3,12 @@ window.addEventListener('load', () => {
   if (portal) {
     portal.addEventListener('click', () => {
       window.location.href = 'https://www.pinterest.com/';
-    });
-  }
+        });
+      }
+}
+)
 
-  function FeedItem(title, body, linkUrl, imageUrl){
+  /*function FeedItem(title, body, linkUrl, imageUrl){
       this.title = title;
       this.body = body;
       this.linkUrl = linkUrl;
@@ -18,18 +20,65 @@ window.addEventListener('load', () => {
     new FeedItem('Thinking of ways to stay Active','You do not like running? Or lifting weights? Pilates is a great form of exercising. It focuses on your core but is a full body workout as well. Trust me you do not want to miss out on the rewarding feeling of being active. Not convinced? Read some benefits of pilates', 'https://www.betterhealth.vic.gov.au/pilates-health-benefits', 'https://images.squarespace-cdn.com/content/v1/619d262b80de15571c7a0a75/f96bc888-5e57-44c8-a165-e0a790fda007/DSC_9942.jpg?format=1500w' ),
     new FeedItem('Plans to make this summer with your Friends','Each summer goes by and you never know what to do? Here is an easy solution to your boredom.', 'https://rusticpathways.com/inside-rustic/online-magazine/15-great-ideas-for-your-summer-bucket-list', 'https://herviewfromhome.com/wp-content/uploads/2018/05/shutterstock_378610153-768x512.jpg'),
   ];
+*/
 
-  function createFeedItemHTML(feedItem) {
+  function createFeedItemHTML(feedsItem, index) {
     return `
       <div class="feed-item">
-        <h3><a href="${feedItem.linkUrl}" target="_blank">${feedItem.title}</a></h3>
-        <img src="${feedItem.imageUrl}" alt="${feedItem.title}">
-        <p>${feedItem.body}</p>
+        <h3><a href="${feedsItem.linkUrl}" target="_blank">${feedsItem.title}</a></h3>
+        <img src="${feedsItem.imageUrl}" alt="${feedsItem.title}">
+        <p>${feedsItem.body}</p>
+        <button class ="delete-button" onclick ="deleteFeedItem(${index})">Delete</button>
       </div>
     `;
   }
 
-  function loadFeed() {
+  function getcurrentFeed() {
+    fetch("/api/feedItem")
+     .then(res => res.json())
+     .then(data => {
+       const newsfeedsElement = document.getElementById("newsfeed");
+    if (newsfeedsElement) {
+      newsfeedsElement.innerHTML = "";
+      data.forEach((feedsItem. index) => {
+        newsfeedsElement.innerHTML += createFeedItemHTML(feedsItem, index);
+      });
+    }
+     })
+  }
+  getcurrentFeed();
+
+  window.deleteFeedItem = function (index) {
+    fetch("/api/feedItem/" + index, {
+      method: 'DELETE',
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.message);
+        getcurrentFeed();
+      })
+      .catch(error => {
+        console.error('Error deleting feed item', error);
+      });
+    };
+
+    const addButton = document.getElementById("add-button");
+
+    if (addButton) {
+      addButton.addEventListener("click", () => {
+        const title = document.getElementById("title").value.trim();
+        const body = document.getElementById("body").value.trim();
+        const linkUrl = document.getElementById("linkUrl").value.trim();
+        const imageUrl = document.getElementById("imageUrl").value.trim();
+
+        const newFeedItem = { title, body, linkUrl, imageUrl};
+      } )
+
+      }
+    
+    
+  
+  /*function loadFeed() {
     const newsfeedElement = document.getElementById('newsfeed');
     if (newsfeedElement) {
       newsfeedElement.innerHTML = currentStories.map(createFeedItemHTML).join('');
@@ -39,3 +88,4 @@ window.addEventListener('load', () => {
   loadFeed();
 });
 
+*/
